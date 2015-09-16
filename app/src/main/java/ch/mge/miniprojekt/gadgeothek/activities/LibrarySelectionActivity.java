a31fe7e41d4b6099930f0946a7fb90cd9d93d87b
oxylius <noah.hendrikx@gmail.com>
2015-09-16 18:29:12 +0200

    Added Autocomplete function to Server Address Added List of Default Servers in strings.xml

--- app/src/main/java/ch/mge/miniprojekt/gadgeothek/activities/LibrarySelectionActivity.java
+++ app/src/main/java/ch/mge/miniprojekt/gadgeothek/activities/LibrarySelectionActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,10 +21,18 @@ public class LibrarySelectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library_selection);
 
+        //Autocomplete TextView
+        AutoCompleteTextView serverAddressElement = (AutoCompleteTextView) findViewById(R.id.serverAddressAutoComplete);
+        String[] defaultServers = getResources().getStringArray(R.array.default_servers_array);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, defaultServers);
+        serverAddressElement.setAdapter(adapter);
+
     }
 
     public void clickSetServer(View view) {
-        EditText serverAddressElement = (EditText) findViewById(R.id.serverAddress);
+
+        AutoCompleteTextView serverAddressElement = (AutoCompleteTextView) findViewById(R.id.serverAddressAutoComplete);
         String serverAddress = serverAddressElement.getText().toString();
         if (serverAddress.trim().length() == 0) {
             Toast.makeText(LibrarySelectionActivity.this, "Please enter an Server Address!", Toast.LENGTH_SHORT).show();
