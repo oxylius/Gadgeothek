9a8f07ab65e5ad21c163157570a37dde84be6bcd
Stefano Kals <stefano.kals@bluewin.ch>
2015-09-28 19:55:25 +0200

    basic loans checker working

--- app/src/main/java/ch/mge/miniprojekt/gadgeothek/activities/LoanUser.java
+++ app/src/main/java/ch/mge/miniprojekt/gadgeothek/activities/LoanUser.java
@@ -1,13 +1,13 @@
 package ch.mge.miniprojekt.gadgeothek.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.List;
 
 import ch.mge.miniprojekt.gadgeothek.R;
+import ch.mge.miniprojekt.gadgeothek.helper.loansAdapter;
 import ch.mge.miniprojekt.gadgeothek.domain.Loan;
 import ch.mge.miniprojekt.gadgeothek.service.Callback;
 import ch.mge.miniprojekt.gadgeothek.service.LibraryService;
@@ -17,13 +17,17 @@ public class LoanUser extends GadgeothekMain {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setActivityTitle("Loans");
+        setActivityTitle("My loaned Gadgets");
         setContentView(R.layout.activity_loan_user);
 
         LibraryService.getLoansForCustomer(new Callback<List<Loan>>() {
             @Override
             public void onCompletion(List<Loan> input) {
                 //https://guides.codepath.com/android/Using-the-RecyclerView
+                RecyclerView rvLoans = (RecyclerView) findViewById(R.id.rvLoans);
+                loansAdapter adapter = new loansAdapter(input);
+                rvLoans.setAdapter(adapter);
+                rvLoans.setLayoutManager(new LinearLayoutManager(LoanUser.this));
             }
 
             @Override
---
+++ app/src/main/java/ch/mge/miniprojekt/gadgeothek/helper/loansAdapter.java
@@ -0,0 +1,69 @@
+package ch.mge.miniprojekt.gadgeothek.helper;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ch.mge.miniprojekt.gadgeothek.R;
+import ch.mge.miniprojekt.gadgeothek.domain.Loan;
+
+/**
+ * Created by kali on 28.09.15.
+ */
+
+public class loansAdapter extends
+    RecyclerView.Adapter<loansAdapter.ViewHolder> {
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView nameTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            nameTextView = (TextView) itemView.findViewById(R.id.item_name);
+        }
+    }
+
+    private List<Loan> mLoans;
+
+    public loansAdapter(List<Loan> loans) {
+        mLoans = loans;
+    }
+
+    @Override
+    public loansAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.item_loans, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(loansAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        Loan loans = mLoans.get(position);
+
+        // Set item views based on the data model
+        TextView textView = viewHolder.nameTextView;
+        textView.setText(loans.getGadget().getName());
+
+    }
+
+    // Return the total count of items
+    @Override
+    public int getItemCount() {
+        return mLoans.size();
+    }
+}
