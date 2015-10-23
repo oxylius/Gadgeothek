d118e719292cbb81d0726be1210f6f6b7d42ba36
oxylius <noah.hendrikx@gmail.com>
2015-10-23 13:28:18 +0200

    More Cleanup

--- app/src/main/java/ch/mge/miniprojekt/gadgeothek/activities/GadgeothekMain.java
+++ app/src/main/java/ch/mge/miniprojekt/gadgeothek/activities/GadgeothekMain.java
@@ -17,8 +17,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.FrameLayout;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import ch.mge.miniprojekt.gadgeothek.R;
